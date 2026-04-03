b7b2c6dd823af58d7a19e69393fea89fd9c470bc
Nitin Khare <nitinkhare@Mac.lan>
2026-04-03 22:08:20 +0530

    second commit

--- episodes/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function EpisodesRedirectPage() {
-  redirect("/series/episodes");
-}
--- page.tsx
+++
@@ -1,5 +0,0 @@
-import { HomeDiscoverySections } from "@/components/home-discovery-sections";
-
-export default function HomePage() {
-  return <HomeDiscoverySections />;
-}
